84889fb7834a82d0c96ebe7c8ff87d35010027e2
root <root@tmp.anywise.cn>
2014-04-25 18:10:13 +0200

    Release!

---
+++ footer.php
@@ -0,0 +1,51 @@
+
+	<footer class="main-footer">
+	
+	<?php if (!Bunyad::options()->disable_footer): ?>
+		<div class="wrap">
+		
+		<?php if (is_active_sidebar('main-footer')): ?>
+			<ul class="widgets row cf">
+				<?php dynamic_sidebar('main-footer'); ?>
+			</ul>
+		<?php endif; ?>
+		
+		</div>
+	
+	<?php endif; ?>
+	
+	
+	<?php if (!Bunyad::options()->disable_lower_footer): ?>
+		<div class="lower-foot">
+			<div class="wrap">
+		
+			<?php if (is_active_sidebar('lower-footer')): ?>
+			
+			<div class="widgets">
+				<?php dynamic_sidebar('lower-footer'); ?>
+			</div>
+			
+			<?php endif; ?>
+		
+			</div>
+		</div>		
+	<?php endif; ?>
+	
+	</footer>
+	
+</div> <!-- .main-wrap -->
+
+<?php wp_footer(); ?>
+
+<script type="text/javascript">
+var disqus_shortname = 'blendernation';
+(function () {
+var s = document.createElement('script'); s.async = true;
+s.type = 'text/javascript';
+s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
+</script>
+
+</body>
+</html>
