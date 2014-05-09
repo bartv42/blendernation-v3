2d167af6c5d34ea321299d090bab3dd53fdd6068
root <root@tmp.anywise.cn>
2014-05-09 17:35:15 +0200

    Replaced PayPal button with link to support page

--- ads/responsive.php
+++ ads/responsive.php
@@ -21,14 +21,17 @@ $bn_adunit++;
 </div>
 
 <div class="adblock-notice" id="bn-adunit-<?=$bn_adunit?>">
-	It looks like you're using an ad blocker. I really need the income to keep this site running, so please enable advertisements on BlenderNation, or leave me a small donation. Everything helps! 
+	It looks like you're using an ad blocker! I really need the income to keep this site running.
+	<br/>If you enjoy BlenderNation and you think it’s a valuable resource to the Blender community, please take a moment to <a href="http://www.blendernation.com/support-blendernation/" style="color:#fff;font-weight:bold;text-decoration:underline;">read how you can support BlenderNation</a>.
 
+<!--
 	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="padding:0px;margin:0px;">
 	<input type="hidden" name="cmd" value="_s-xclick">
 	<input type="hidden" name="hosted_button_id" value="ELYFS4VZXGQBQ">
 	<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" style="padding:0px;margin:5px 0 0 0;background:none;border:none;">
 	<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 	</form>	
+-->
 	
 </div>
 
